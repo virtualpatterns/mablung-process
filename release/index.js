66e1ae479f096b99dbf3b7de8c9da1c483ea1485
virtualpatterns.com <code@virtualpatterns.com>
2021-07-02 09:35:50 -0400

    removing release from git

--- release/index.js
+++
@@ -1,7 +0,0 @@
-export * from './library/process.js';
-export * from './library/error/duration-exceeded-process-error.js';
-export * from './library/error/option-not-supported-process-error.js';
-export * from './library/error/pid-file-exists-process-error.js';
-export * from './library/error/pid-file-not-exists-process-error.js';
-
-//# sourceMappingURL=index.js.map
