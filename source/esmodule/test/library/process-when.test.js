0d17def498f78e3f7ced90a9d6f63a1f303721f6
virtualpatterns.com <code@virtualpatterns.com>
2022-04-28 19:59:03 -0400

    updates

--- source/esmodule/test/library/process-when.test.js
+++ source/esmodule/test/library/process-when.test.js
@@ -7,35 +7,35 @@ const PollInterval = parseInt(Math.ceil(MaximumDuration / 4.00))
 Test('when(..., ..., ...) using a synchronous function that returns true', async (test) => {
   test.truthy(await Process.when(() => {
     return true
-  }, MaximumDuration, PollInterval))
+  }, PollInterval, MaximumDuration))
 })
 
 Test('when(..., ..., ...) using an asynchronous function that returns true', async (test) => {
   test.truthy(await Process.when(() => {
     return Promise.resolve(true)
-  }, MaximumDuration, PollInterval))
+  }, PollInterval, MaximumDuration))
 })
 
 Test('when(..., ..., ...) using a synchronous function that returns false', async (test) => {
   await test.throwsAsync(Process.when(() => {
     return false
-  }, MaximumDuration, PollInterval), { 'instanceOf': ProcessDurationExceededError })
+  }, PollInterval, MaximumDuration), { 'instanceOf': ProcessDurationExceededError })
 })
 
 Test('when(..., ..., ...) using a synchronous function that fails', async (test) => {
   await test.throwsAsync(Process.when(() => {
     throw new Error()
-  }, MaximumDuration, PollInterval), { 'instanceOf': Error })
+  }, PollInterval, MaximumDuration), { 'instanceOf': Error })
 })
 
 Test('when(..., ..., ...) using an asynchronous function that returns false', async (test) => {
   await test.throwsAsync(Process.when(() => {
     return Promise.resolve(false)
-  }, MaximumDuration, PollInterval), { 'instanceOf': ProcessDurationExceededError })
+  }, PollInterval, MaximumDuration), { 'instanceOf': ProcessDurationExceededError })
 })
 
 Test('when(..., ..., ...) using an asynchronous function that fails', async (test) => {
   await test.throwsAsync(Process.when(() => {
     return Promise.reject(new Error())
-  }, MaximumDuration, PollInterval), { 'instanceOf': Error })
+  }, PollInterval, MaximumDuration), { 'instanceOf': Error })
 })
